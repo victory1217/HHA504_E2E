4a3acf4af22fbea2fc467ae98a3cca1ec56e10f6
Victoria Rodriguez <victoria.inbox77@gmail.com>
2021-12-14 22:14:18 -0500

    Updated Trigger

--- HHA504_E2E.py
+++ HHA504_E2E.py
@@ -34,7 +34,7 @@ SELECT * FROM H1N1_Flu_Vaccines LIMIT 15;
 DELIMITER $$
 CREATE TRIGGER H1N1_concern_trigger BEFORE INSERT ON H1N1_Flu_Vaccines
 FOR EACH ROW 
-BEGIN IF NEW.alert <=3 THEN
+BEGIN IF NEW.alert >=3 THEN
 SIGNAL SQLSTATE '45000'
 SET MESSAGE_TEXT = 'H1N1 concern should be a numerical value between 0 and 3. Please try again.'
 ;END IF;
